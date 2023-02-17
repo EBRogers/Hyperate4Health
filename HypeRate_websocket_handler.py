432cf35b831804fd8e3f2861fbf951438822bba2
EBRogers <ethanbtrogers@gmail.com>
2023-02-17 12:02:48 -0500

    Updated README.md
    For HypeRate_websocket_handler.py:
    - Added typing
    - changed auto naming convention to be YYYYMMDD from YYYYDDMM (?? not sure why I did that in the first place)
    - streamlines parameter validation
    - refactored to allow for recorder instantiation without automatically starting websocket
    - made internal methods "private" with _
    - added the rest of the cli arguments to parser
    - removed dotenv as a required package

--- HypeRate_websocket_handler.py
+++ HypeRate_websocket_handler.py
@@ -4,43 +4,51 @@ import os
 import time
 from csv import writer
 from threading import Timer
-
+from typing import Optional
 import pandas as pd
 import rel
 import websocket
-from dotenv import load_dotenv
 
 
 # This program was written by Ethan Rogers (Personal Health Informatics, Northeastern University, Boston, MA)
+# It is distributed ** AS IS ** under the MIT license. See 'License' in README.md
 # https://github.com/EBRogers/Hyperate4Health
 # Special thanks to Hendrikje Wagner (HypeRate COO) and the HypeRate Team (HypeRate.io)
 # Their work enables our research.
 
-class recorder:
+class Recorder:
 
-    def __init__(self, hyperate_id: str, save_path: str, timeout: int = 30):
-
-        # Parameter Validation
-        try:
-            self.api_key = os.environ['API_KEY']
-        except KeyError:
-            raise AttributeError("You must set the API key as an environmental variable. This can be loaded "
-                                 "automatically by placing a '.env' file in the working directory of this program, "
-                                 "and with the text: API_KEY = \"[PUT YOUR KEY HERE]\"")
-        if type(hyperate_id) != str:
-            raise AttributeError("ID must be a string.")
-
-        if type(save_path) != str:
-            raise AttributeError("save_path must be a string.")
-
-        if type(timeout) != int:
+    def __init__(
+            self,
+            hyperate_id: str,
+            save_path: str,
+            timeout: int = 30,
+            api_key: Optional[str] = None
+    ):
+        # Parameter Validation: API Key
+        self.api_key = api_key or os.environ.get('HYPERATE_API_KEY')
+        if not self.api_key:
+            raise AttributeError(
+                "You must either pass the API Key as the parameter 'api_key' or set the API key as an environmental "
+                "variable. This can be loaded automatically by placing a '.env' file in the working directory of this "
+                "program, and with the text: API_KEY = \"[PUT YOUR KEY HERE]\" and calling load_dotenv() from the "
+                "'dotenv' packege"
+            )
+        # Parameter Validation: Hyperate ID
+        if not isinstance(hyperate_id, str):
+            raise TypeError(
+                "ID must be a string like 'AB12' or 'XY89' and is generated when you run the HypeRate app on your smartphone or measurement device.")
+        # Parameter Validation: save path
+        if not isinstance(save_path, str):
+            raise TypeError("save_path must be a string.")
+        # Parameter Validation: Timeout
+        if not isinstance(timeout, int):
             try:
                 timeout = int(timeout)
-            except TypeError:
-                raise AttributeError("ID must be a string.")
+            except ValueError:
+                raise TypeError("ID must be a string.")
         if timeout < 0 or timeout > 86400:
-            raise AttributeError("Timeout can't be less than 0 or more than a day.")
-
+            raise ValueError("Timeout can't be less than 0 or more than a day.")
         # Init some vars
         self.id = hyperate_id
         self.timeout = timeout
@@ -51,33 +59,31 @@ class recorder:
         self.last_hr_ts = 0
         self.start_time = time.time()
         self.data = pd.DataFrame(columns=["UNIX_TIMESTAMP", "HR"])
-
         # Determine the name and save path, create save dir, and file to save to
         if save_path.endswith(".csv"):
             self.save_path = save_path
         else:
-            file_name = f"HR_{self.id}_{time.strftime('%Y%d%m_%H%M')}.csv"
+            file_name = f"HR_{self.id}_{time.strftime('%Y%m%d_%H%M')}.csv"
             self.save_path = os.path.join(save_path, file_name)
-        if not os.path.exists(os.path.dirname(self.save_path)):
-            os.makedirs(os.path.dirname(self.save_path))
+        os.makedirs(os.path.dirname(self.save_path), exist_ok=True)
         with open(self.save_path, 'w') as f_object:
             writer_object = writer(f_object)
             writer_object.writerow(['UNIX_TIMESTAMP', 'HR'])
-            f_object.close()
 
         # make the websocket and run it for a while
-        self.ws = self.make_websock()
-        self.run_websock()
+    def begin_recording(self):
+        self.ws = self._make_websock()
+        self._run_websock()
 
     # Websocket Starters
-    def make_websock(self):
+    def _make_websock(self):
         return websocket.WebSocketApp(self.address,
-                                      on_open=self.on_open,
-                                      on_message=self.on_message,
-                                      on_error=self.on_error,
-                                      on_close=self.on_close)
+                                      on_open=self._on_open,
+                                      on_message=self._on_message,
+                                      on_error=self._on_error,
+                                      on_close=self._on_close)
 
-    def run_websock(self):
+    def _run_websock(self):
         self.ws.run_forever(dispatcher=rel)
         # self.ws.run_forever(dispatcher=rel, reconnect=5)
         # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
@@ -85,7 +91,7 @@ class recorder:
         rel.dispatch()
 
     # Websocket on_action Function
-    def on_message(self, ws, message):
+    def _on_message(self, ws, message):
         # print(f"on_message: {message}")
         m = json.loads(message)
         if m['event'] == "hr_update":
@@ -99,7 +105,6 @@ class recorder:
                 with open(self.save_path, 'a') as f_object:
                     writer_object = writer(f_object)
                     writer_object.writerow([time.time(), m['payload']['hr']])
-                    f_object.close()
             except Exception as e:
                 print(e)
                 print(
@@ -108,19 +113,19 @@ class recorder:
                 self.data.to_csv(self.save_path, index=False)
                 self.stop()
 
-    def on_error(self, ws, error):
+    def _on_error(self, ws, error):
         # print('on_error: ON ERROR CALLED')
         print(error)
 
-    def on_close(self, ws, close_status_code, close_msg):
+    def _on_close(self, ws, close_status_code, close_msg):
         # print("on_close: ### closed ###")
         self.stop()
 
-    def on_open(self, ws):
+    def _on_open(self, ws):
         # print("on_open: Opened connection")
         ws.send(json.dumps(self.params))
         # print("on_open: timer_set")
-        self.start_beats()
+        self._start_beats()
         print(
             f"Connection to Hyperate established with ID {self.id}\n(i) For a better visual go to: app.hyperate.io/{self.id}")
 
@@ -139,11 +144,8 @@ class recorder:
         except websocket._exceptions.WebSocketConnectionClosedException:
             self.stop()
 
-    def get_last_message_ts(self):
-        return self.data.UNIX_TIMESTAMP.max()
-
     # Beat Initializers
-    def start_beats(self):
+    def _start_beats(self):
         self.response_beat = Timer(20, self.respond_to_keep_open)
         self.close_beat = Timer(self.timeout, self.stop)
         self.response_beat.start()
@@ -182,11 +184,15 @@ if __name__ == "__main__":
                                                     "already, it will be overriden.")
     # argument
     parser.add_argument('--timeout', type=int, default=30, required=False,
-                        help='Optional. This argument specfies the minimum time allowed between HR signals for the '
+                        help='Optional. This argument specifies the minimum time allowed between HR signals for the '
                              'connection to remain open and the program to be running.')
+    parser.add_argument('--api_key', type=str, default=None, required=False,
+                        help='Optional. This argument provides the API key to the recorder. If you do not specify, '
+                             'it will be automatically loaded from the "HYPERATE_API_KEY" environmental variable')
     args = parser.parse_args()
-    load_dotenv()
+
     websocket.enableTrace(False)
-    w = recorder(args.id, args.save_path, timeout=args.timeout)
+    w = Recorder(args.id, args.save_path, timeout=args.timeout)
+    w.begin_recording()
 
     # hyperate_id: str, save_path: str, timeout: int = 15
